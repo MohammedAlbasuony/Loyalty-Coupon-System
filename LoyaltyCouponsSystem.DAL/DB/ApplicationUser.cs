d957adb9d87ec7ec1432975912693586b3cf58e1
itzAhmedSamir <as3644893@gmail.com>
2025-01-03 08:52:06 +0200

    Registration updates

--- LoyaltyCouponsSystem.DAL/DB/ApplicationUser.cs
+++ LoyaltyCouponsSystem.DAL/DB/ApplicationUser.cs
@@ -12,7 +12,10 @@ namespace LoyaltyCouponsSystem.DAL.DB
     {
         public string? FullName { get; set ; }
         public string? Role { get; set; }
-        public string? Phone { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Governorate { get; set; }
+        public string? City { get; set; }
+        public string? NationalID { get; set; }
         public bool? IsDeleted { get; set; } = false;
         public string? Imagepath { get; set; }
         public virtual Admin Admin { get; set; }
