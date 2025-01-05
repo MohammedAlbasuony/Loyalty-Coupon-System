c0458c3062058d4f6e8f841b2e1a3209ceb4f682
itzAhmedSamir <as3644893@gmail.com>
2025-01-05 18:38:07 +0200

    Changing Email with Name plus some features in admin panel

--- LoyaltyCouponsSystem.BLL/ViewModel/Account/ChangePasswordViewModel.cs
+++ LoyaltyCouponsSystem.BLL/ViewModel/Account/ChangePasswordViewModel.cs
@@ -9,9 +9,8 @@ namespace LoyaltyCouponsSystem.BLL.ViewModel.Account
 {
     public class ChangePasswordViewModel
     {
-        [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
--- LoyaltyCouponsSystem.BLL/ViewModel/Account/LoginViewModel.cs
+++ LoyaltyCouponsSystem.BLL/ViewModel/Account/LoginViewModel.cs
@@ -9,9 +9,8 @@ namespace LoyaltyCouponsSystem.BLL.ViewModel.Account
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
