8926169fbf0c47088ffe19d57ffa6f17e2277315
Mohammed Albasuony <mido.beso.hosny@gmail.com>
2025-01-21 16:06:01 +0200

    Permission 1/4 Done

---
+++ LoyaltyCouponsSystem.DAL/Entity/Permission/Permissions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoyaltyCouponsSystem.DAL.Entity.Permission
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<RolePermission> RolePermissions { get; set; }
+
+    }
+
+}
